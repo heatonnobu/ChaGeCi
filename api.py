76694075e0e06f9dd0d292986163b77a92c7b7dc
heatonnobu <zhengwei@zhengweideMacBook-Pro.local>
2015-09-20 03:16:51 +0800

    因为不是所有的歌都有lyric文件，所以修改取词为网页取词 专辑创作者信息也都从同一个网页中抓取

--- api.py
+++ api.py
@@ -9,25 +9,74 @@ import urllib.request
 import urllib.parse
 import time
 import re
+import logging
+from werkzeug.contrib.fixers import ProxyFix
 
 #sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+#logging.basicConfig(filename='logs/api.log', level=logging.INFO, format='[%(asctime)s %(levelname)s $(message)s', datefmt='%Y%m%d %H:%M:%S')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return "Hello World"
+        return "Hello World" if request.args.get('echostr')==None else request.args.get('echostr')
     if request.method == 'POST':
         soup = BeautifulSoup(request.data, 'xml')
-
+        searchString = ''
+        if soup.MsgType.string == 'voice':
+            soup.MsgType.string = 'text'
+            soup.Recognition.name = 'Content'
         searchString = soup.Content.string
         returnString = xiami(str(searchString))
 
         soup.ToUserName.string, soup.FromUserName.string = CData(soup.FromUserName.string), CData(soup.ToUserName.string)
         soup.CreateTime.string = CData(str(int(time.time())))
         soup.Content.string = CData(returnString)
+
         print(soup)
         return Response(str(soup), mimetype='text/xml')
+
+@app.route('/test/<keyword>', methods=['GET', 'POST'])
+def test(keyword):
+    logging.info('start')
+    # result = xiami(keyword)
+    logging.error('end')
+    result = xiamitest(keyword)
+    return result
+
+def xiamitest(keyword):
+    data = None
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"}
+    #1 http://www.xiami.com/search?pos=1&key={keyword} html
+    print('keyword', urllib.parse.quote(keyword))
+    req1 = urllib.request.Request('http://www.xiami.com/search?pos=1&key=' + urllib.parse.quote(keyword), data, headers)
+    response1 = urllib.request.urlopen(req1)
+    xiami_soup1 = BeautifulSoup(response1.read().decode('gbk', 'ignore').encode('utf-8'), 'html.parser')
+    td_songs = xiami_soup1.find_all('td', class_='song_name')
+    print('td_songs', td_songs)
+    for td in td_songs:
+        a = td.find('a')
+        if str(a['href'])[:4] != 'http':
+            a = a.next_sibling.next_sibling
+        print(a)
+        print(1)
+        req2 = urllib.request.Request(a['href'], data, headers)
+        response2 = urllib.request.urlopen(req2)
+        print(2)
+        xiami_soup2 = BeautifulSoup(response2.read(), 'html.parser')
+        lrc_main = xiami_soup2.find('div', class_='lrc_main')
+        if lrc_main != None:
+            info = xiami_soup2.find(id='albums_info').get_text()
+            print(info)
+            info = info.replace('：\n', '：')
+            print(info)
+            info = info.replace('\n\n\n', '\n')
+            geci = info + lrc_main.get_text().strip()
+            print(3)
+            print('geci1', geci)
+            return geci
+    return "未找到歌词"
 
 
 '''
@@ -42,38 +91,62 @@ def xiami(keyword):
     data = None
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"}
     #1 http://www.xiami.com/search?pos=1&key={keyword} html
-    print('keyword', urllib.parse.quote(keyword))
     req1 = urllib.request.Request('http://www.xiami.com/search?pos=1&key=' + urllib.parse.quote(keyword), data, headers)
     response1 = urllib.request.urlopen(req1)
+    xiami_soup1 = BeautifulSoup(response1.read().decode('gbk', 'ignore').encode('utf-8'), 'html.parser')
+    td_songs = xiami_soup1.find_all('td', class_='song_name')
+    for td in td_songs:
+        a = td.find('a')
+        if str(a['href'])[:4] != 'http':
+            a = a.next_sibling.next_sibling
+        req2 = urllib.request.Request(a['href'], data, headers)
+        response2 = urllib.request.urlopen(req2)
+        xiami_soup2 = BeautifulSoup(response2.read(), 'html.parser')
+        lrc_main = xiami_soup2.find('div', class_='lrc_main')
+        if lrc_main != None:
+            info = xiami_soup2.find(id='albums_info').get_text().replace('：\n', '： ').replace('\n\n\n', '\n')
+            geci = info + lrc_main.get_text().strip()
+            return geci
+    return "未找到歌词"
+
+# http://s.music.qq.com/fcgi-bin/yqq_search_v8_cp?tab=all|<keyword>&w=<keyword>
+@app.route('/test/qq/<keyword>', methods=['GET', 'POST'])
+def qq(keyword):
+    data = None
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"}
+    #1 http://www.xiami.com/search?pos=1&key={keyword} html
+    keyword = urllib.parse.quote(keyword)
+    print('url', 'http://s.music.qq.com/fcgi-bin/yqq_search_v8_cp?tab=all|%s&w=%s' % (keyword, keyword))
+    req1 = urllib.request.Request('http://s.music.qq.com/fcgi-bin/yqq_search_v8_cp?tab=all|%s&w=%s' % (keyword, keyword), data, headers)
+    response1 = urllib.request.urlopen(req1)
+    print(response1.read().decode(encoding='UTF-8'))
     xiami_soup1 = BeautifulSoup(response1.read(), 'html.parser')
-    a1 = xiami_soup1.find('td', class_='song_name').find('a')
-    if str(a1['href'])[:4] != 'http':
-        a1 = a1.next_sibling.next_sibling
-    songid = str(a1['href']).split('/')[-1]
-    print('song', songid)
-
-    #2 http://www.xiami.com/song/playlist/id/{song} xml
-    req2 = urllib.request.Request('http://www.xiami.com/song/playlist/id/' + songid, data, headers)
-    response2 = urllib.request.urlopen(req2)
-    xiami_soup2 = BeautifulSoup(response2.read(), 'xml')
-    lyric_url = str(xiami_soup2.lyric_url.string)
-    print('lyric_url', lyric_url)
-
-    #3 lyric doc
-    if re.match(r'^http.*', lyric_url):
-        print('match')
-        req3 = urllib.request.Request(lyric_url, data, headers)
-        response3 = urllib.request.urlopen(req3)
-        lyric_bytes = response3.read()
-        print('lyric_before', lyric_bytes)
-        lyric_string = lyric_bytes.decode(encoding='utf-8')
-        print('lyric_string', lyric_string)
-
-        lyric_string = re.sub(r'\[.*?\]', '', lyric_string)
-    else:
-        lyric_string = '未找到歌词'
-
-    return str(lyric_string)
+    print(xiami_soup1.find('ol'))
+    div_songs = xiami_soup1.find('ol', id='divsonglist').find_all('div', class_='music_name')
+    print('div_songs', div_songs)
+    for div in div_songs:
+        a = div.span.a
+        # if str(a['href'])[:4] != 'http':
+        #     a = a.next_sibling.next_sibling
+        print(a)
+        print(1)
+        req2 = urllib.request.Request(a['href'], data, headers)
+        response2 = urllib.request.urlopen(req2)
+        print(2)
+        return 'hehe'
+        xiami_soup2 = BeautifulSoup(response2.read(), 'html.parser')
+        lrc_main = xiami_soup2.find('div', class_='lrc_main')
+        if lrc_main != None:
+            info = xiami_soup2.find(id='albums_info').get_text()
+            print(info)
+            info = info.replace('：\n', '：')
+            print(info)
+            info = info.replace('\n\n\n', '\n')
+            geci = info + lrc_main.get_text().strip()
+            print(3)
+            print('geci1', geci)
+            return geci
+    return "未找到歌词"
 
 
 if __name__ == '__main__':
